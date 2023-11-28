440616d4e1e78997ccb4837096f1427e93c6a265
Natalia <nany.nagisa@gmail.com>
2023-11-28 00:10:38 -0300

    Header Ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './components/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap'; 
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <NavLink to="/" className="navbar-brand">
+            Ecommerce
+          </NavLink>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <NavLink to="/cart" className="nav-link">
+                <FaShoppingCart /> Carrito{' '}
+                <Badge variant="primary">0</Badge>
+              </NavLink>
+              <NavLink to="/login" className="nav-link">
+                <FaUser /> Iniciar sesión
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
