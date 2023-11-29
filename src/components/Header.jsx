9e12f137acd8ce3def4259ed5dccad9a621ba53e
Natalia <nany.nagisa@gmail.com>
2023-11-29 15:10:40 -0300

    Modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <NavLink to="/" className="navbar-brand">
-            Ecommerce
+            Mi Ecommerce
           </NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
